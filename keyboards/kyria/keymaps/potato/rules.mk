e49b1db8b0186487ca54d018e15d8fce8cfabdfe
Stefan Schokker <stefan.schokker@icloud.com>
2021-04-04 15:58:45 +0200

    Move keymap to potato folder, change MCU to proton-c

---
+++ keyboards/kyria/keymaps/potato/rules.mk
@@ -0,0 +1,8 @@
+# Convert to QMK-PROTON-C
+CTPC = yes
+
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # ENables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+AUTO_SHIFT_ENABLE = yes	   # Enable autoshift
+WPM_ENABLE = yes		   # Enable WPM counter
